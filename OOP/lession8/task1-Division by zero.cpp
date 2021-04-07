0827bf8eb5d8134c60e1951a0ae3f29eb5079f6a
Gelrush <gelrush@icloud.com>
2021-04-07 09:47:10 +0300

    Added OOP:less8(exception):tasks

---
+++ OOP/lession8/task1-Division by zero.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+template<typename T>
+T myDiv(T num, T den)
+{
+    if (den == 0) throw std::invalid_argument("Error: Division by zero!");
+
+    return num / den;
+}
+
+int main(int argc, char const* argv[])
+{
+    int res{ 0 };
+
+    try
+    {
+        res = myDiv(1, 0);
+    }
+    catch (std::invalid_argument e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    std::cout << "result = " << res << std::endl;
+
+
+    return 0;
+}
---
+++ OOP/lession8/task2-Custom exception.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class Ex
+{
+public:
+    int x;
+
+    Ex(int num)
+        : x(num)
+    {   }
+};
+
+class Bar
+{
+private:
+    int y{ 0 };
+
+public:
+    void setY(int a)
+    {
+        if (y + a > 100) throw Ex(y * a);
+
+        y = a;
+    }
+};
+
+
+int main(int argc, char const* argv[])
+{
+    Bar value;
+
+    try
+    {
+        int num;
+        do
+        {
+            std::cout << "waiting for input...\n";
+            std::cin >> num;
+            value.setY(num);
+        } while (num);
+    }
+    catch (Ex& e)
+    {
+        std::cerr << "Error: Input result - " << e.x << '\n';
+    }
+
+    return 0;
+}
