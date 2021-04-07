0b5bf48d081a15c3b16e0585e2858120c4821c9d
Gelrush <gelrush@icloud.com>
2021-04-07 09:39:47 +0300

    Added OOP:less7-8:lection - unique_ptr, exception

---
+++ OOP/lession7/AutoPtr(unique_ptr).cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+
+/* Неявное преобразование классов
+*  Умные указатели */
+
+class Bar
+{
+private:
+    int m_x;
+public:
+    Bar() {   }                         // конструктор по умолчанию
+    Bar(int x) : m_x(x) {   }           // конструктор с параметром
+    explicit Bar(std::string str) {  }  // конструктор с параметром запрещающий неявное преобразование
+    Bar(const Bar& b) { m_x = b.m_x; }  // конструктор копирования
+    void operator=(const Bar& b) { m_x = b.m_x; }   // оператор присваивания
+
+    int getX() { return m_x; }
+    void print() { std::cout << m_x << std::endl; }
+};
+
+
+template<typename T>
+class AutoPtr
+{
+private:
+    T* m_ptr;
+public:
+    AutoPtr(const T* ptr) : m_ptr(ptr) { }
+    AutoPtr(const AutoPtr&) = delete;
+    void operator=(const AutoPtr&) = delete;
+    //{ m_ptr = ptr.m_ptr; }      // поверхностное копирование (память одна, ссылки две) - плохо
+
+    AutoPtr(AutoPtr&& p)    // контруктор перемещения (используя r-value ссылку)
+    {
+        m_ptr = p.m_ptr;
+        p.m_ptr = nullptr;
+    }
+    void operator=(AutoPtr&& p) // оператор перемещения (передаваемая переменная больше не ссылается на данные)
+    {
+        m_ptr = p.m_ptr;
+        p.m_ptr = nullptr;
+    }
+    
+    ~AutoPtr() { delete m_ptr; }
+
+    T* operator->() { return m_ptr; }
+    T& operator*() { return &m_ptr; }
+
+};
+
+int main(int argc, char const* argv[])
+{
+    {
+        Bar bar; // конструктор по умолчанию
+        Bar bar2{ 5 }; // конструктор с параметром
+        Bar bar3 = bar2; // конструктор копирования
+        Bar bar4;
+        bar3 = bar2; // оператор присваивания
+        Bar bar4 = 'c'; // конструктор с параметров (неявное преобразование char в Bar(int) )
+        std::string str = "c";
+        Bar bar5 = str; // explicit запрещает неявное преобразование
+    }
+    {       // Пользовательский умный указатель
+        //Bar* bar = new Bar();
+        AutoPtr<Bar> bar(new Bar());
+        bar->getX();
+        Bar b = *bar;
+
+        AutoPtr<Bar> bar2 = std::move(bar); // функция std::move создает r-value ссылку объекта Bar, вызывая контруктор перемещения
+        bar = std::move(bar2);  // обратно, используя оператор перемещения
+
+        //...
+        int x = 4;
+        //...
+        if (x > 0)
+        {
+            //delete bar;
+            exit(0);
+        }
+        //delete bar;
+    }
+    {       // std:: версии
+        std::unique_ptr<Bar> bar = std::make_unique<Bar>(4); // = new Bar(4)
+        bar->print();
+
+        std::unique_ptr<Bar> bar2 = std::move(bar);
+
+        if (bar) std::cout << "not null" << std::endl;
+        else std::cout << "null" << std::endl;  // this
+
+        bar.release(); // возвращает указатель и удаляет этот указатель из unique_ptr
+        bar.get(); // возвращает указатель из unique_ptr
+        bar.reset(); // удаляет указатель из unique_ptr
+
+        std::shared_ptr<Bar> bar3 = std::make_shared<Bar>(5);
+            // создаетсяумный указатель для нескольких переменных. 
+        // При удалении одного из объектов память освобождаться не будет. 
+        // Только при удалении всех переменных указывающих на одну и ту же переменную память будет освобождена
+    }
+
+
+    return 0;
+}
---
+++ OOP/lession8/Exception.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <cmath>
+
+/* Механизм исключительных ситуаций*/
+
+double mySqrt(double a)
+{
+    // Если пользователь ввёл отрицательное число, то выбрасываем исключение
+    if (a < 0.0)
+        throw "Can not take sqrt of negative number"; // выбрасывается исключение типа const char*
+
+    return sqrt(a);
+}
+
+class Parent        // Функциональный try-блок - для обработки исключений полученных из конструкторов родительских классов!!!
+{
+private:
+    int m_age;
+public:
+    Parent(int age) : m_age(age)
+    {
+        if (age <= 0)
+            throw 1;
+    }
+    virtual void print() { std::cout << "Parent"; }
+};
+
+class Child : public Parent
+{
+public:
+    Child(int age) try : Parent(age) // обратите внимание на ключевое слово try здесь
+    {   }
+    catch (...) // Этот блок находится на том же уровне отступа, что и конструктор
+    {
+        // Исключения из списка инициализации членов класса Child или тела конструктора обрабатываются здесь
+
+        std::cerr << "Construction of Parent failed\n";
+        // Если мы здесь не будем явно выбрасывать исключение, то текущее (пойманное) исключение будет повторно сгенерировано и отправлено в стек вызовов
+    }
+    virtual void print() { std::cout << "Child"; }
+};
+
+
+
+int main(int argc, char const* argv[])
+{
+            // ТИПЫ ИСКЛЮЧЕНИЙ
+    try
+    {
+        throw -1;      // генерация исключения типа int
+        
+        enum ER { ENUM_INVALID_INDEX };
+        throw ENUM_INVALID_INDEX;       // генерация исключения типа enum
+
+        throw "Cannot take square root of negative number"; // генерация исключения типа const char* (строка C-style)
+        
+        double dX;
+        throw dX;   // генерация исключения типа double (переменная типа double, которая была определена ранее)
+        
+        class MyException : public std::exception { };
+        throw MyException();   // генерация исключения с использованием объекта класса MyException
+    }
+    catch (int a)
+    {
+        std::cerr << "We caught an int exception with value" << a << '\n';
+    }
+
+
+
+            // РАСКРУЧИВАНИЕ СТЕКА
+    double a = -1.1;
+    try 
+    {
+        double d = mySqrt(a); // выбросит исключение
+        std::cout << "The sqrt of " << a << " is " << d << '\n';
+    }
+    catch (const char* exception) // обработка исключений типа const char*
+    {
+        std::cerr << "Error: " << exception << std::endl;
+    }
+
+            // CATCH-ALL
+    try
+    {
+        throw 7; // выбрасывается исключение типа int
+    }
+    catch (double a)
+    {
+        std::cerr << "We caught an exception of type double: " << a << '\n';
+    }
+    catch (...) // обработчик catch-all
+    {   // Обработчик catch-all должен быть последним в цепочке блоков catch
+        std::cerr << "We caught an exception of an undetermined type!\n";
+    }
+
+
+        
+            // std::exception
+    try
+    {
+        // Здесь код с использованием стандартной библиотеки С++
+    }
+    // Этот обработчик ловит bad_alloc и все дочерние ему классы-исключения
+    catch (std::bad_alloc& exception)
+    {
+        std::cerr << "You ran out of memory!" << '\n';
+    }
+    // Этот обработчик ловит exception и все дочерние ему классы-исключения
+    catch (std::exception& exception)
+    {
+        std::cerr << "Standard exception: " << exception.what() << '\n';
+    }
+
+
+
+                // Повторная генерация исключений
+    try
+    {
+        try
+        {
+            throw Child(1);
+        }
+        catch (Parent& p)
+        {
+            std::cout << "Caught Parent p, which is actually a ";
+            p.print();
+            std::cout << "\n";
+            throw; // Мы здесь повторно выбрасываем исключение
+        }
+    }
+    catch (Parent& p)
+    {
+        std::cout << "Caught Parent p, which is actually a ";
+        p.print();
+        std::cout << "\n";
+    }
+
+
+
+                // Функциональный try-блок - для обработки исключений полученных из конструкторов родительских классов!!!
+    try
+    {
+        Child child(0);
+    }
+    catch (int)
+    {
+        std::cout << "Oops!\n";
+    }
+
+
+
+    return 0;
+}
